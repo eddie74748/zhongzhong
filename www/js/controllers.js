201ee4f560cbe3044fa4f773fc87efcb35368a57
ltloveandroid <ltloveandroid@gmail.com>
2015-06-11 18:06:57 +0800

    image grid in zhongzong tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('ZhoneCtrl', function($scope) {})
+.controller('ZhoneCtrl', function($scope) {
+    $scope.onSlideClicked = function(slideIndex)
+    {
+
+    }
+
+    $scope.topPhotos = [{
+        "url": "http://zcr7.ncfstatic.com/attachment/201506/08/10/5574f9b47cd34o_600x450.jpg"
+    },{
+        "url": "http://zcr6.ncfstatic.com/attachment/201504/30/16/6ec406e1e8d19a648cc743a32659981754o_600x450.jpg"
+    },{
+        "url": "http://zcr5.ncfstatic.com/attachment/201505/22/16/78cc35d716ee3f0dc4af7460484c581a78o_600x450.jpg"
+    },{
+        "url": "http://zcr7.ncfstatic.com/attachment/201506/08/10/5574f9b47cd34o_600x450.jpg"
+    },{
+        "url": "http://zcr5.ncfstatic.com/attachment/201505/22/16/78cc35d716ee3f0dc4af7460484c581a78o_600x450.jpg"
+    },{
+        "url": "http://zcr6.ncfstatic.com/attachment/201504/30/16/6ec406e1e8d19a648cc743a32659981754o_600x450.jpg"
+    },{
+        "url": "http://zcr7.ncfstatic.com/attachment/201506/08/10/5574f9b47cd34o_600x450.jpg"
+    },{
+        "url": "http://zcr7.ncfstatic.com/attachment/201506/08/10/5574f9b47cd34o_600x450.jpg"
+    }];
+})
 
 .controller('BuyCtrl', function($scope, Products) {
   // With the new view caching in Ionic, Controllers are only called
@@ -15,6 +38,33 @@ angular.module('starter.controllers', [])
   $scope.remove = function(product) {
       Products.remove(product);
   }
+
+    $scope.doRefresh = function()
+    {
+        $http.get('/new-products')
+            .success(function(newProducts) {
+                $scope.products = newProducts;
+            })
+            .finally(function() {
+                // Stop the ion-refresher from spinning
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+    }
+
+    $scope.loadMore = function() {
+        $http.get('/more-products').success(function(products) {
+            appendProducts(products);
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
+    $scope.moreDataCanBeLoaded = function() {
+        return true;
+    };
+
+    $scope.$on('$stateChangeSuccess', function() {
+        $scope.loadMore();
+    });
 })
 
 .controller('BuyDetailCtrl', function($scope, $stateParams, Products) {
